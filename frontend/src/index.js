c169d9c2ade898894d47a76496e66b75108a6eec
Vishal Venkatesh <venvis1998@gmail.com>
2025-06-16 15:55:15 +0530

    FE: Added redux to UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import { App } from "./App.tsx";
 import reportWebVitals from "./reportWebVitals";
 import "../src/assets/styles/bootstrap.custom.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
