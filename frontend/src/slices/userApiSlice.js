c6df6091b06983cd8bee54cf7670d4cbcd3c112d
Vishal Venkatesh <venvis1998@gmail.com>
2025-06-24 14:44:56 +0530

    Added User API Slice

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,16 @@
+import { apiSlice } from "./apiSlice.js";
+import { USERS_URL } from "../constants.js";
+
+const endpoints = (builder) => ({
+  login: builder.mutation({
+    query: (credentials) => ({
+      url: `${USERS_URL}/login`,
+      method: "POST",
+      body: credentials,
+    }),
+  }),
+});
+
+export const userApiSlice = apiSlice.injectEndpoints({ endpoints });
+
+export const { useLoginMutation } = userApiSlice;
