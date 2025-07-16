2e10f2f141b7279aaefa9e71b68e92db42f81670
Vishal Venkatesh <venvis1998@gmail.com>
2025-07-16 20:47:50 +0530

    Clearing cart when user logout

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -16,7 +16,7 @@ const authSlice = createSlice({
     },
     removeCredentials: (state) => {
       state.userInfo = null;
-      localStorage.removeItem("userInfo");
+      localStorage.clear();
     },
   },
 });
