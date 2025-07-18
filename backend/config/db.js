35e2bbddd5585fe925250ef25580221506396479
Vishal Venkatesh <venvis1998@gmail.com>
2025-07-18 07:54:21 +0530

    Production deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 const DbConnection = async () => {
   try {
-    var connect = await mongoose.connect(process.env.MONGO_URI);
+    var connect = await mongoose.connect(
+      process.env.MONGO_URI_PROD || process.env.MONGO_URI
+    );
     console.log(`Mongo Db connection successful: ${connect.connection.host}`);
   } catch (error) {
     console.log(
--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,6 @@ app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-enco
 app.use(cookieParser()); // Middleware to parse cookies for authentication
 
 const PORT = process.env.PORT;
-app.get("/health", (_req, res) => {
-  res.send(`Server is running in port: ${PORT}`);
-});
 
 app.use("/api/products", products);
 app.use("/api/users", user);
@@ -34,6 +31,23 @@ app.use("/api/reviews", reviews);
 // __dirname is used to get the current directory of the script
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the React frontend app
+  // The build directory contains the production-ready React app
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // Handle any requests that don't match the above routes
+  // This will serve the React app for any route that is not handled by the API routes
+  // This is useful for single-page applications (SPAs) where the frontend handles routing
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"));
+  });
+} else {
+  app.get("/health", (_req, res) => {
+    res.send(`Server is running in port: ${PORT}`);
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
